1c533f229277713cebe196287bf99a1ab0b84a72
Daniel Lam <Dann.lam@gmail.com>
2023-08-25 23:24:59 -0700

    Updated comments

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 import "chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm";
 import Navbar from "./components/Navbar";
 // import Footer from "./components/Footer";
+//
 import Display from "./pages/Display";
-
 // import TickerMenu from "./components/TickerMenu";
 
+//App -- main container
+//Display -- container for majority of functions.
 export default function App() {
   return (
     <div className="App flex flex-col min-h-screen items-center">
